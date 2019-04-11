cd4d0ba21a7cedda61c2532270458a96d383a9c7
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-04-11 05:28:07 -0400

    added in the bookmarks crud

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
 
-  has_many :topics
+  has_many :topics, dependent: :destroy
 
   validates :password, presence: true, length: { minimum: 6 }
   # validates :email, presence: true, uniqueness: { case_sensitive: true }, length: { minimum: 3, maximum: 254 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
+  get 'bookmarks/show'
+  get 'bookmarks/new'
+  get 'bookmarks/edit'
   devise_for :users
 
-  resources :topics
+  resources :topics do
+    resources :bookmarks, except: [:index]
+  end
+  
   get "topics/:id/edit" => "topics#edit"
 
   get 'about' => 'welcome#about'
