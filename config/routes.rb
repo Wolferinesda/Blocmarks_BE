15c52ce821daa839abd7a1ecdd304e8463503aa3
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-04-12 13:12:37 -0400

    removing worthless code and adding validations to bookmarks model

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'bookmarks/show'
-  get 'bookmarks/new'
-  get 'bookmarks/edit'
   devise_for :users
 
   resources :topics do
     resources :bookmarks, except: [:index]
   end
-  
+
   get "topics/:id/edit" => "topics#edit"
 
   get 'about' => 'welcome#about'
